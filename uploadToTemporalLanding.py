dd39537f888e30322c72cabf3b201bc09959b643
mionaD-upc <miona.dimic@estudiantat.upc.edu>
2023-04-22 10:40:49 +0200

    update

--- uploadToTemporalLanding.py
+++ uploadToTemporalLanding.py
@@ -25,8 +25,8 @@ def local_files(dir_name):
         for file in files:
             full_path = os.path.join(root, file)
             all_files.append(full_path)
-    print("length", len(all_files))
-    print(all_files)
+    # print("length", len(all_files))
+    # print(all_files)
     return all_files
 
 
@@ -38,11 +38,12 @@ def hdfs_files(dir_name):
         print(f"The folder {dir_name} does not exist.")
         hdfs_cli.makedirs(dir_name)
         print(f"The folder {dir_name} was created.")
+    f_formats = []
     f_paths = [psp.join(dpath, f_name)
                for dpath, _, f_names in hdfs_cli.walk(dir_name)
                for f_name in f_names]
-    print(f_paths)
-    return f_paths
+    return f_paths, f_formats
+
 
 
 def progress_callback(file_name, bytes_uploaded):
@@ -63,13 +64,13 @@ delete_hdfs_directory('/user/bdm/temporal_landing')
 #                 overwrite=True)
 
 hdfs_cli.upload('/user/bdm/temporal_landing', '/Users/miona.dimic/Desktop/MDS/Q2 2023/BDM/Project/BDM_P1/data/', progress=progress_callback,
-                overwrite=True)
+             overwrite=True)
 
 
 # show hdfs and local files
-print("Files in HDFS: ")
+# print("Files in HDFS: ")
 hdfs_files('/user/bdm/temporal_landing')
-# local_files('/Users/Desktop/data/')
-#local_files('/Users/miona.dimic/Desktop/MDS/Q2 2023/BDM/Project/BDM_P1/data/')
+# # local_files('/Users/Desktop/data/')
+# #local_files('/Users/miona.dimic/Desktop/MDS/Q2 2023/BDM/Project/BDM_P1/data/')
 
 
